b75c203c8b03ed7eea87a0d67cc2d2d3ddc50ce8
Marlrero <mrtj0329@gmail.com>
2022-10-06 17:25:14 +0900

    2022-10-06-5

--- tkinter_primary_adv/cat_game/game_20220929.py
+++ tkinter_primary_adv/cat_game/game_20220929.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import random
 
 BG_SIZE = (912, 768)
 CUR_LIMIT = 24
@@ -6,27 +7,32 @@ BLOCK_SIZE = (8, 10)
 BLOCK_PIXEL = 72
 
 cat_loc = [
-    [1, 0, 0, 0, 0, 0, 7, 7],
-    [0, 2, 0, 0, 0, 0, 7, 7],
-    [0, 0, 3, 0, 0, 0, 0, 0],
-    [0, 0, 0, 4, 0, 0, 0, 0],
-    [0, 0, 0, 0, 5, 0, 0, 0],
-    [0, 0, 0, 0, 0, 6, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 2, 3, 4, 5, 6]
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0]
 ]
 
 cursor_x = 0  # 커서의 좌표
 cursor_y = 0
 mouse_x = 0   # 실제 마우스 좌표
 mouse_y = 0
+mouse_c = 0   # 마우스 클릭 여부
 
 def mouse_move(e): # 마우스 움직일 때 이벤트 함수
     global mouse_x, mouse_y
     mouse_x = e.x
     mouse_y = e.y
+    
+def mouse_press(e): # 마우스 클릭 시 이벤트 함수
+    global mouse_c
+    mouse_c = 1
 
 def drop_cat(): # 고양이가 위에서 떨어져야 함
     for y in range(BLOCK_SIZE[0], -1, -1): # 8부터 0까지 1씩 감소
@@ -43,6 +49,12 @@ def draw_cat():
             if cat_loc[y][x] > 0:
                 cvs.create_image(x*BLOCK_PIXEL + 60, y*BLOCK_PIXEL + 60, \
                     image=IMG_CAT[cat_loc[y][x]], tag="CAT")
+                
+                if mouse_c == 1: # 마우스를 클릭했으면
+                    mouse_c = 0  # 마우스 클릭 변수 초기화
+                    # 커서 위치 칸에 무작위 고양이 놓기
+                    cat_loc[cursor_y][cursor_x] = random.randint(1, 6) 
+
 
 def main_action(): # 실시간 처리 함수
     global cursor_x, cursor_y
@@ -85,6 +97,7 @@ cursor = tk.PhotoImage(file="resource/neko_cursor.png")
 cvs.create_image(BG_SIZE[0] // 2, BG_SIZE[1] // 2, image=background)
 
 root.bind("<Motion>", mouse_move) # 마우스 이벤트 처리
+root.bind("<ButtonPress>", mouse_press)
 main_action()  # 실시간 처리 함수 시작점
 
 root.mainloop()
