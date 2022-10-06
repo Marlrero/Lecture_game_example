fd5bb11c6cdf334238ce9e68ed41d4b95bb8bc93
Marlrero <mrtj0329@gmail.com>
2022-10-06 17:29:45 +0900

    2022-10-06-7

--- tkinter_primary_adv/cat_game/game_20220929.py
+++ tkinter_primary_adv/cat_game/game_20220929.py
@@ -48,21 +48,22 @@ def draw_cat():
         for x in range(BLOCK_SIZE[0]):
             if cat_loc[y][x] > 0:
                 cvs.create_image(x*BLOCK_PIXEL + 60, y*BLOCK_PIXEL + 60, \
-                    image=IMG_CAT[cat_loc[y][x]], tag="CAT")
-                
-                if mouse_c == 1: # 마우스를 클릭했으면
-                    mouse_c = 0  # 마우스 클릭 변수 초기화
-                    # 커서 위치 칸에 무작위 고양이 놓기
-                    cat_loc[cursor_y][cursor_x] = random.randint(1, 6) 
+                    image=IMG_CAT[cat_loc[y][x]], tag="CAT") 
 
 
 def main_action(): # 실시간 처리 함수
     global cursor_x, cursor_y
     drop_cat()
+    
     if CUR_LIMIT <= mouse_x < CUR_LIMIT + BLOCK_PIXEL * BLOCK_SIZE[0] \
         and CUR_LIMIT <= mouse_y < CUR_LIMIT + BLOCK_PIXEL * BLOCK_SIZE[1]:
         cursor_x = int((mouse_x - CUR_LIMIT) / BLOCK_PIXEL)
         cursor_y = int((mouse_y - CUR_LIMIT) / BLOCK_PIXEL)
+    
+    if mouse_c == 1: # 마우스를 클릭했으면
+        mouse_c = 0  # 마우스 클릭 변수 초기화
+        # 커서 위치 칸에 무작위 고양이 놓기
+        cat_loc[cursor_y][cursor_x] = random.randint(1, 6)
     
     cvs.delete('CAT')
     cvs.delete("CURSOR")
