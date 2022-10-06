28702468d7bb5d64b8aa6cfdac0f4844f9fd7145
Marlrero <mrtj0329@gmail.com>
2022-10-06 16:51:05 +0900

    2022-10-06

--- tkinter_primary_adv/cat_game/game_20220929.py
+++ tkinter_primary_adv/cat_game/game_20220929.py
@@ -15,6 +15,19 @@ def mouse_move(e): # 마우스 움직일 때 이벤트 함수
     mouse_x = e.x
     mouse_y = e.y
 
+def main_action(): # 실시간 처리 함수
+    global cursor_x, cursor_y
+    if CUR_LIMIT <= mouse_x < CUR_LIMIT + BLOCK_PIXEL * BLOCK_SIZE[0] \
+        and CUR_LIMIT <= mouse_y < CUR_LIMIT + BLOCK_PIXEL * BLOCK_SIZE[1]:
+        cursor_x = int((mouse_x - CUR_LIMIT) / BLOCK_PIXEL)
+        cursor_y = int((mouse_y - CUR_LIMIT) / BLOCK_PIXEL)
+    
+    cvs.delete("CURSOR")
+    cvs.create_image(cursor_x * BLOCK_PIXEL + 60, cursor_y * BLOCK_PIXEL + 60,\
+                     image=cursor, tag="CURSOR")
+    root.after(100, main_action)
+
+
 root = tk.Tk()
 root.title("AniPang")
 root.resizable(False, False)
@@ -25,5 +38,8 @@ cvs.pack()
 background = tk.PhotoImage(file="resource/neko_bg.png")
 cursor = tk.PhotoImage(file="resource/neko_cursor.png")
 cvs.create_image(BG_SIZE[0] // 2, BG_SIZE[1] // 2, image=background)
+
+root.bind("<Motion>", mouse_move) # 마우스 이벤트 처리
+main_action()  # 실시간 처리 함수 시작점
 
 root.mainloop()
